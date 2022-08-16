8aadfac1bea5d20c177571c4b442fb41fdcb0bb3
Marshall Buck <dev@marshallbuck.com>
2022-08-16 17:44:39 -0500

    added getChains

--- markov.js
+++ markov.js
@@ -10,12 +10,13 @@ class MarkovMachine {
     // include things like "The", "cat", "cat.".
     this.words = text.split(/[ \r\n]+/);
     this.chains = this.getChains();
+
   }
 
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,13 +24,28 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
     // TODO: implement this!
-  }
 
+    // iterate through the array of words
+    //figure out if threat key is already there
+
+    const chainMap = new Map;
+
+    for (let i = 0; i < this.words.length; i++) {
+
+      if (!chainMap.get(this.words[i])) {
+        chainMap.set(this.words[i], [this.words[i + 1]]);
+      } else {
+        chainMap.set[this.words[i], chainMap.get(this.words[i]).push(this.words[i + 1])];
+      }
+    }
+
+    return chainMap;
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
@@ -42,3 +58,9 @@ class MarkovMachine {
     // - repeat until reaching the terminal null
   }
 }
+
+const catInHatMachine = new MarkovMachine("the cat in the hat");
+// // debugger;
+// console.log(catInHatMachine.chains.forEach((k, v) => console.log(k, v)));
+
+
